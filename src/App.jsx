a714a91919a5fff8a98f7faae7292f94afa0cd9c
Muhammad Shakeel <shakeel7521951@gmail.com>
2025-04-25 11:41:15 +0500

    routing setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,35 @@
-import './App.css'
+// App.jsx
+import "./App.css";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import About from "./pages/About";
 
-const MainLayout = ()=>{
-  return(
-    <div>
-      
-    </div>
-  )
-}
-
-function App() {
+const MainLayout = () => {
   return (
     <div>
-
+      <Navbar />
+      <Outlet />
+      <Footer />
     </div>
-  )
+  );
+};
+
+// Set up router with routes
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/about", element: <About /> },
+    ],
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
