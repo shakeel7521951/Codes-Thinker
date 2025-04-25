e64c8b9951caf162ee601affe6810c45624a4247
Muhammad Shakeel <shakeel7521951@gmail.com>
2025-04-25 11:34:12 +0500

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+const MainLayout = ()=>{
+  return(
+    <div>
+      
+    </div>
+  )
+}
+
+function App() {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default App
