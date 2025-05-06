dfef1cde3407c1bea093dd01ddffb555a81ed815
Muhammad Shakeel <shakeel7521951@gmail.com>
2025-05-06 16:49:07 +0500

    update

--- src/components/home/MovingServices.jsx
+++ src/components/home/MovingServices.jsx
@@ -1,5 +1,5 @@
 import { FaBootstrap, FaCode, FaFigma, FaPhp, FaReact, FaSearch, FaWordpress } from "react-icons/fa";
-import '../../../../Codes-Thinker/src/App.css';
+// import '../../../../Codes-Thinker/src/App.css';
 import { MdJavascript } from "react-icons/md";
 import { RiTailwindCssFill } from "react-icons/ri";
 import { DiDjango } from "react-icons/di";
