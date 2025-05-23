2cc1fb12221e00ab14788c4a6ff095c5f2942d97
fakharabbasbhatti <fakharabbasbhatti333@gmail.com>
2025-05-22 22:57:06 -0700

    about pages change

--- src/components/about/CareerPathways.jsx
+++ src/components/about/CareerPathways.jsx
@@ -1,39 +1,39 @@
 import React from 'react'
-import img from '../../../public/about/about.png'
+import submit from '../../../public/about/submit.jpg'
+import expert from '../../../public/about/Experts.jpg'
+import personal from '../../../public/about/personal.jpg'
+import seamless from '../../../public/about/seamless.jpg'
+
 export default function CareerPathways() {
     const aray = [
-        {
-            image: img, title: "Submit Application"
-        },
-        {
-            image: img, title: "Expert Evaluation"
-        },
-        {
-            image: img, title: "Personal Interview"
-        },
-        {
-            image: img, title: "Seamless Onboarding"
-        },
+        { image: submit, title: "Submit Application" },
+        { image: expert, title: "Expert Evaluation" },
+        { image: personal, title: "Personal Interview" },
+        { image: seamless, title: "Seamless Onboarding" },
     ]
+
     return (
-        <div className=' p-4 sm:p10'>
-            <h1 className="text-center text-2xl sm:text-4xl font-bold mb-10  text-[#0F00AA]">Your Path to Joining Code's Thinker</h1>
+        <div className='p-4 sm:p-10'>
+            <h1 className="text-center text-2xl sm:text-4xl font-bold mb-10 text-[#0F00AA]">
+                Your Path to Joining Code's Thinker
+            </h1>
 
-
-            <div className=' grid grid-cols-1 gap-4 sm:grid-cols-3 md:grid-cols-4'>
-                {
-                    aray.map((item, index) => (
-                        <div key={index + 1} className='mx-auto'>
-                            <div className=' mx-auto hover:border-dotted  group overflow-hidden border-3 border-black  h-40 w-40 rounded-full'>
-                                <img src={item.image} alt={item.title} className=' scale-110 transition-all ease-in-out duration-100 group-hover:scale-95 h-full w-full rounded-full' />
-                            </div>
-                            <div className=' mt-2'>
-                                <span className=' font-bold text-md sm:text-xl'>{index + 1}-</span>
-                                <span className=' font-bold text-md sm:text-xl'>{item.title}</span>
-                            </div>
+            <div className='grid grid-cols-1 gap-4 sm:grid-cols-3 md:grid-cols-4'>
+                {aray.map((item, index) => (
+                    <div key={index} className='mx-auto'>
+                        <div className='mx-auto group overflow-hidden border-3 border-black h-40 w-40 rounded-full hover:border-dotted'>
+                            <img
+                                src={item.image}
+                                alt={item.title}
+                                className='scale-110 transition-all ease-in-out duration-100 group-hover:scale-95 h-full w-full rounded-full'
+                            />
                         </div>
-                    ))
-                }
+                        <div className='mt-2'>
+                            <span className='font-bold text-md sm:text-xl'>{index + 1}- </span>
+                            <span className='font-bold text-md sm:text-xl'>{item.title}</span>
+                        </div>
+                    </div>
+                ))}
             </div>
         </div>
     )
